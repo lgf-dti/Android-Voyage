3ec3827ef58c0288a019451b358b880d8303ee5f
TISSOT <david.tissot@ac-grenoble.fr>
2024-09-12 15:22:04 +0200

    add: (MainActivity) Gestion événement créer un Kit de voyage.

--- app/src/main/java/com/example/voyage/MainActivity.java
+++ app/src/main/java/com/example/voyage/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.voyage;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,14 +16,46 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Déclaration des attributs de l'IHM
+    EditText editTextDepart;
+    EditText editTextDestination;
+    Button buttonCreerKit;
+    Button buttonAjouterOption;
+
+    KitVoyage kitVoyage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        buttonCreerKit = findViewById(R.id.buttonValiderKit);
+        editTextDepart = findViewById(R.id.editTextDepart);
+        editTextDestination = findViewById(R.id.editTextDestination);
+        buttonAjouterOption = findViewById(R.id.buttonAjouterOption);
+
+        buttonCreerKit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String depart = editTextDepart.getText().toString();
+                String destination = editTextDestination.getText().toString();
+                kitVoyage = new KitVoyage(depart, destination);
+                Log.i("kit", kitVoyage.toString());
+                Toast.makeText(MainActivity.this, kitVoyage.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        buttonAjouterOption.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
         // Création d'un kit de voyage
-        KitVoyage kit = new KitVoyage("Marseille", "Istanbul");
+    /*    KitVoyage kit = new KitVoyage("Marseille", "Istanbul");
         Sejour sejour = new Sejour("Hotel 4* a Istanbul", 40, 5, 100);
         Transport transport = new Transport("Vol vers Istanbul", 240);
         kit.ajouterOption(sejour);
@@ -27,6 +63,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Affichage du kit de voyage dans Logcat
         Log.i("kit",kit.toString());
+*/
 
     }
 }
